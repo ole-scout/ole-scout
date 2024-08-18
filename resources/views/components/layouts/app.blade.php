0f290a85d862e15a0fc715a539d53a6385343f79
Alan Plum <alan.plum@foss-haas.de>
2024-08-18 18:48:24 +0200

    Use layout for welcome page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html
+    lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    dir="{{ __('filament-panels::layout.direction') ?? 'ltr' }}"
+>
     <head>
         <meta charset="utf-8" />
-
         <meta name="application-name" content="{{ config('app.name') }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -17,12 +19,26 @@
 
         @filamentStyles
         @vite('resources/css/app.css')
+        @stack('styles')
+        <script>
+            const theme = localStorage.getItem('theme') ?? 'system';
+
+            if (
+                theme === 'dark' ||
+                (theme === 'system' &&
+                    window.matchMedia('(prefers-color-scheme: dark)')
+                        .matches)
+            ) {
+                document.documentElement.classList.add('dark');
+            }
+        </script>
     </head>
 
-    <body class="antialiased">
+    <body class="min-h-screen antialiased font-normal bg-gray-50 text-gray-950 dark:bg-gray-950 dark:text-white">
         {{ $slot }}
-
+        <x-consent::consent-modal />
         @filamentScripts
         @vite('resources/js/app.js')
+        @stack('scripts')
     </body>
 </html>
