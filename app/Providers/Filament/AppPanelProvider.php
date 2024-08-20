399ea51858cca39f99f9ae960cb90ab97c7b4f35
Alan Plum <alan.plum@foss-haas.de>
2024-08-20 18:48:46 +0200

    Remove filament info widget

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -77,7 +77,6 @@ class AppPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
