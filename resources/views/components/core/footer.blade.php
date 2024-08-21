55d3219e9e9cefb8105c7d7eac51444716cabe6e
Alan Plum <alan.plum@foss-haas.de>
2024-08-21 18:17:29 +0200

    Use x-filament::link

--- resources/views/components/core/footer.blade.php
+++ resources/views/components/core/footer.blade.php
@@ -2,21 +2,40 @@
   $settings = app(\FossHaas\Consent\Settings\AppConsentSettings::class);
 @endphp
 <footer class="px-2 pb-24 space-y-8 md:p-8 lg:text-sm min-h-24 lg:min-h-16">
-  <nav class="flex flex-wrap items-center justify-center space-x-1 md:space-x-2">
+  <nav class="flex flex-wrap items-center justify-center space-x-1 md:space-x-2 *:p-1 *:block">
     @isset($settings->imprint_url)
-    <x-link class="block p-1 text-gray-700 md:px-2 dark:text-gray-200 hover:underline" href="{{ $settings->imprint() }}">{{ __('Impressum') }}</x-link>
+    <x-link
+      weight="normal"
+      color="gray"
+      href="{{ $settings->imprint() }}"
+    >{{ __('Impressum') }}</x-link>
     @endisset
-    <x-link class="block p-1 text-gray-700 md:px-2 dark:text-gray-200 hover:underline" href="{{ $settings->privacyPolicy() }}">{{ __('Datenschutz') }}</x-link>
-    <x-link class="block p-1 text-gray-700 md:px-2 dark:text-gray-200 hover:underline" href="{{ $settings->consent() }}">{{ __('Datenschutz-Einstellungen') }}</x-link>
+    <x-link
+      weight="normal"
+      color="gray"
+      href="{{ $settings->privacyPolicy() }}"
+    >{{ __('Datenschutz') }}</x-link>
+    <x-link
+      weight="normal"
+      color="gray"
+      href="{{ $settings->consent() }}"
+    >{{ __('Datenschutz-Einstellungen') }}</x-link>
   </nav>
   <div class="flex justify-center">
-    <x-link href="https://ole-scout.app" class="flex items-center justify-center text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white group">
-      @svg('icon-ole-scout', 'w-7 h-7 mx-1 fill-current')
-      <span class="ml-1 mr-3 text-xs" lang="en">
+    <x-link
+      href="https://ole-scout.app"
+      class="w-full opacity-50 hover:opacity-100 *:!no-underline *:leading-5 !gap-2 [&>svg]:mt-1 [&>svg]:size-7"
+      weight="normal"
+      color="white"
+      icon="icon-ole-scout"
+      lang="en"
+    >
       powered by OLE Scout<br>
-      made with <span class="group-hover:text-red-500 group-focus:text-red-500" aria-label="love">♥️</span>
-      in <abbr lang="de" class="no-underline" title="Ostwestfalen-Lippe">OWL</abbr>
-      </span>
+      made with <x-filament::icon
+        icon="heroicon-m-heart"
+        class="inline-block pb-px size-3 group-hover/link:text-red-500 group-focus/link:text-red-500"
+        aria-label="love"
+      /> in <abbr lang="de" class="no-underline" title="Ostwestfalen-Lippe">OWL</abbr>
     </x-link>
   </div>
 </footer>
--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -3,8 +3,8 @@
   $isExternal = substr($href, 0, 1) !== '/';
   if (!$isExternal) $href = url($href);
 @endphp
-<a href="{{ $href }}" {{ $attributes->merge($isExternal ? [
+<x-filament::link href="{{ $href }}" {{ $attributes->merge($isExternal ? [
   'target' => '_blank',
   'rel' => 'noopener noreferrer',
   'class' => 'external-link',
-] : []) }}>{{ $slot }}</a>
+] : []) }}>{{ $slot }}</x-filament::link>
