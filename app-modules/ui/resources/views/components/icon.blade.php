2da1168de31e6d21e1c5199d5bb71212bdb74fa2
Alan Plum <alan.plum@foss-haas.de>
2024-09-05 14:32:29 +0200

    Tweak icon a11y

--- app-modules/ui/resources/views/components/icon.blade.php
+++ app-modules/ui/resources/views/components/icon.blade.php
@@ -3,14 +3,23 @@
     'size' => 'md',
 ])
 @php
-    $icon = 'fluentui-' . $icon . '-' . (match($size) {
-        'sm' => '16',
-        default => '20',
-        'lg' => '24',
-    });
+    if (substr($icon, 0, 1) === '/') {
+        $icon = substr($icon, 1);
+    } else {
+        $icon = 'fluentui-' . $icon . '-' . (match($size) {
+            'sm' => '16',
+            default => '20',
+            'lg' => '24',
+        });
+    }
 @endphp
 @svg($icon, $attributes->filter(
     fn ($value) => $value !== null && $value !== false
+)->merge(
+    $attributes->hasAny('aria-label', 'aria-labelledby') ? [
+    ] : [
+        'aria-hidden' => 'true',
+    ]
 )->class(['icon', match($size) {
     'sm' => 'icon-sm',
     default => null,
