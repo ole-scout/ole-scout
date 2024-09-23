a7b5ef3a7579e0c189c874e3619bfa98588530b5
Alan Plum <alan.plum@foss-haas.de>
2024-09-23 15:07:12 +0200

    Add temporary link to admin panel

--- app-modules/courses/resources/views/livewire/dashboard.blade.php
+++ app-modules/courses/resources/views/livewire/dashboard.blade.php
@@ -1,3 +1,4 @@
-<div>
-    <x-ui::button :href="route('courses.root')">{{ __('Course overview') }}</x-ui::button>
-</div>
+<ul>
+    <li><x-ui::button :href="route('courses.root')">{{ __('Course overview') }}</x-ui::button></li>
+    <li><x-ui::button href="/admin">{{ __('Admin panel') }}</x-ui::button></li>
+</ul>
