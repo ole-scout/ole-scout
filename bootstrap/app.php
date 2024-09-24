e082e4d2f3ed988feb3c908dc1cc004df440832d
Alan Plum <alan.plum@foss-haas.de>
2024-09-24 09:53:55 +0200

    Redirect guests to login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->prependToGroup("web", AutoLocale::class);
         $middleware->prependToGroup("web", ConsensualCookies::class);
+        $middleware->redirectGuestsTo('/login');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
