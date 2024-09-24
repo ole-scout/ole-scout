373bc9d6fc3da75d1ca9ea426ac5b3402ee913df
Alan Plum <alan.plum@foss-haas.de>
2024-09-24 09:27:53 +0200

    Remove welcome view

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use FossHaas\Auth\Livewire\Authentication;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::get('/login', Authentication::class)->name('login');
