b134c066ed5b148b02de726d86a8e08e63e55c71
Alan Plum <alan.plum@foss-haas.de>
2024-09-24 09:33:29 +0200

    Fix auth middleware use in courses routes
    Middleware call needs to come first or else auth middleware has no effect.

--- app-modules/courses/routes/web.php
+++ app-modules/courses/routes/web.php
@@ -3,12 +3,22 @@
 use FossHaas\Courses\Livewire\CourseGroupRootView;
 use FossHaas\Courses\Livewire\CourseGroupView;
 use FossHaas\Courses\Livewire\DashboardView;
+use FossHaas\Courses\Models\CourseGroup;
 use Illuminate\Support\Facades\Route;
 
-Route::name('courses.')->group(function () {
-    Route::get('/', DashboardView::class)->name('dashboard');
-    Route::get('/g', CourseGroupRootView::class)->name('root');
-    Route::get('/g/{courseGroup:slug}', CourseGroupView::class)->name('group');
-    // Route::get('/c/{course:slug}', CourseView::class)->name('course');
-    // Route::get('/c/{course:slug}/a/{activity:id}', ActivityView::class)->name('activity');
-})->middleware('auth');
+Route::name('courses.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/', DashboardView::class)
+            ->name('dashboard');
+
+        Route::get('/g', CourseGroupRootView::class)
+            ->middleware('can:viewAny,' . CourseGroup::class)
+            ->name('root');
+
+        Route::get('/g/{courseGroup:slug}', CourseGroupView::class)
+            ->middleware('can:view')
+            ->name('group');
+        // Route::get('/c/{course:slug}', CourseView::class)->name('course');
+        // Route::get('/c/{course:slug}/a/{activity:id}', ActivityView::class)->name('activity');
+    });
