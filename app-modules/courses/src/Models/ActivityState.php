7dfdcfec0684fc7eadec908962c6560893ae504b
Alan Plum <alan.plum@foss-haas.de>
2024-09-28 23:30:25 +0200

    Extend activity state model

--- app-modules/courses/src/Models/ActivityState.php
+++ app-modules/courses/src/Models/ActivityState.php
@@ -3,10 +3,25 @@
 namespace FossHaas\Courses\Models;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class ActivityState extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'course_id',
+        'activity_id',
+        'content_state',
+        'completed_at',
+    ];
+
+    protected $casts = [
+        'content_state' => AsArrayObject::class,
+    ];
+
     protected static function booted()
     {
         static::updated(function (ActivityState $activityState) {
@@ -15,23 +30,41 @@ class ActivityState extends Model
                     ->where('user_id', $activityState->user_id)
                     ->first();
                 $activityStates = $courseState->activityState()
-                    ->select(['completed_at'])
+                    ->select(['activity_id', 'completed_at'])
                     ->get();
-                $total = $activityStates->count();
-                $completed = $activityStates
-                    ->filter(fn($state) => $state->completed_at !== null)
+                $numTotal = $activityStates->count();
+                $numCompleted = $activityStates
+                    ->where('completed_at', '!==', null)
                     ->count();
-                if ($completed === $total) {
-                    $courseState->percent_completed = 100;
-                    $courseState->completed_at = now();
-                } else {
-                    $courseState->percent_completed = (int) floor(
-                        ($completed / $total) * 100
-                    );
+                $courseState->percent_completed = (int) round(
+                    ($numCompleted / $numTotal) * 100
+                );
+                if (
+                    $courseState->completed_at === null
+                    && $activityState->activity->is_required
+                ) {
+                    $requiredActivities = $courseState->activities()
+                        ->where('is_required', true)
+                        ->select(['id'])
+                        ->pluck('id');
+                    $requiredCompleted = $activityStates
+                        ->whereIn('activity_id', $requiredActivities)
+                        ->where('completed_at', '!==', null)
+                        ->count();
+                    if ($requiredCompleted === $requiredActivities->count()) {
+                        $courseState->completed_at = now();
+                    }
                 }
                 $courseState->save();
             }
         });
+    }
+
+    public function scopeForUser(Builder $query, ?User $user = null): void
+    {
+        if (!$user) $user = Auth::user();
+        if (!$user) $query->where(false);
+        else $query->where('user_id', $user->id);
     }
 
     public function user()
